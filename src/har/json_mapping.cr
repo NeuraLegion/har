df14320775b4bf3d32f4e9ac5746bbbf3f0e8a9c
Bar Hofesh <bar.hofesh@gmail.com>
2018-03-20 21:16:16 +0200

    cookie expiery is null or String

--- src/har/json_mapping.cr
+++ src/har/json_mapping.cr
@@ -105,7 +105,7 @@ module HAR
       value: String,
       path: String?,
       domain: String?,
-      expires: String,
+      expires: String?,
       httpOnly: Bool?,
       secure: Bool?,
       comment: String?,

a6bc378ab0fba564c77902350218641e01e16ce3
Bar Hofesh <bar.hofesh@gmail.com>
2019-01-06 11:55:48 +0200

    move to JSON::Serializable

--- src/har/json_mapping.cr
+++ src/har/json_mapping.cr
@@ -3,190 +3,189 @@ require "json"
 module HAR
   # Main Data Structure
   class Data
-    JSON.mapping(
-      log: Log
-    )
+    include JSON::Serializable
+
+    property log : Log
   end
 
   class Log
-    JSON.mapping(
-      version: String,
-      creator: Creator,
-      browser: Browser?,
-      pages: Array(Pages)?,
-      comment: String?,
-      entries: Array(Entries),
-    )
+    include JSON::Serializable
+
+    property version : String
+    property creator : Creator
+    property browser : Browser?
+    property pages : Array(Pages)?
+    property comment : String?
+    property entries : Array(Entries)
   end
 
   class Creator
-    JSON.mapping(
-      name: String,
-      version: String,
-      comment: String?,
-    )
+    include JSON::Serializable
+
+    property name : String
+    property version : String
+    property comment : String?
   end
 
   class Browser
-    JSON.mapping(
-      name: String,
-      version: String,
-      comment: String?,
-    )
+    include JSON::Serializable
+
+    property name : String
+    property version : String
+    property comment : String?
   end
 
   class Pages
-    JSON.mapping(
-      startedDateTime: String,
-      id: String,
-      title: String,
-      pageTimings: PageTimings,
-      comment: String?
-    )
+    include JSON::Serializable
+
+    property startedDateTime : String
+    property id : String
+    property title : String
+    property pageTimings : PageTimings
+    property comment : String?
   end
 
   class PageTimings
-    JSON.mapping(
-      onContentLoad: Int32? | Float32? | Float64?,
-      onLoad: Int32? | Float32? | Float64?,
-      comment: String?,
-    )
+    include JSON::Serializable
+
+    property onContentLoad : Int32? | Float32? | Float64?
+    property onLoad : Int32? | Float32? | Float64?
+    property comment : String?
   end
 
   class Entries
-    JSON.mapping(
-      pageref: String?,
-      startedDateTime: String,
-      time: Int32? | Float32? | Float64?,
-      request: Request,
-      response: Response,
-      cache: Cache?,
-      timings: Timings? | Array(String)?,
-      serverIPAddress: String?,
-      connection: String?,
-      comment: String?
-    )
+    include JSON::Serializable
+
+    property pageref : String?
+    property startedDateTime : String
+    property time : Int32? | Float32? | Float64?
+    property request : Request
+    property response : Response
+    property cache : Cache?
+    property timings : Timings? | Array(String)?
+    property serverIPAddress : String?
+    property connection : String?
+    property comment : String?
   end
 
   class Request
-    JSON.mapping(
-      method: String,
-      url: String,
-      httpVersion: String,
-      cookies: Array(Cookie),
-      headers: Array(Header),
-      queryString: Array(QueryString),
-      postData: PostData?,
-      headersSize: Int32?,
-      bodySize: Int32?,
-      comment: String?,
-      fragments: Array(String)?
-    )
+    include JSON::Serializable
+
+    property method : String
+    property url : String
+    property httpVersion : String
+    property cookies : Array(Cookie)
+    property headers : Array(Header)
+    property queryString : Array(QueryString)
+    property postData : PostData?
+    property headersSize : Int32?
+    property bodySize : Int32?
+    property comment : String?
+    property fragments : Array(String)?
   end
 
   class Response
-    JSON.mapping(
-      status: Int32,
-      statusText: String,
-      httpVersion: String,
-      cookies: Array(Cookie),
-      headers: Array(Header),
-      content: Content,
-      redirectURL: String,
-      headersSize: Int32?,
-      bodySize: Int32?,
-      comment: String?,
-    )
+    include JSON::Serializable
+
+    property status : Int32
+    property statusText : String
+    property httpVersion : String
+    property cookies : Array(Cookie)
+    property headers : Array(Header)
+    property content : Content
+    property redirectURL : String
+    property headersSize : Int32?
+    property bodySize : Int32?
+    property comment : String?
   end
 
   class Cookie
-    JSON.mapping(
+    include JSON::Serializable
 
-      name: String,
-      value: String,
-      path: String?,
-      domain: String?,
-      expires: String | Hash(String, String) | Nil,
-      httpOnly: Bool?,
-      secure: Bool?,
-      comment: String?,
-    )
+    property name : String
+    property value : String
+    property path : String?
+    property domain : String?
+    property expires : String | Hash(String, String) | Nil
+    property httpOnly : Bool?
+    property secure : Bool?
+    property comment : String?
   end
 
   class Header
-    JSON.mapping(
-      name: String,
-      value: String,
-      comment: String?,
-    )
+    include JSON::Serializable
+
+    property name : String
+    property value : String
+    property comment : String?
   end
 
   class QueryString
-    JSON.mapping(
-      name: String,
-      value: String,
-      comment: String?
-    )
+    include JSON::Serializable
+
+    property name : String
+    property value : String
+    property comment : String?
   end
 
   class PostData
-    JSON.mapping(
-      mimeType: String?,
-      params: Array(Param)?,
-      text: String,
-      comment: String?
-    )
+    include JSON::Serializable
+
+    property mimeType : String?
+    property params : Array(Param)?
+    property text : String
+    property comment : String?
   end
 
   class Param
-    JSON.mapping(
-      name: String,
-      value: String?,
-      fileName: String?,
-      contentType: String?,
-      comment: String?
-    )
+    include JSON::Serializable
+
+    property name : String
+    property value : String?
+    property fileName : String?
+    property contentType : String?
+    property comment : String?
   end
 
   class Content
-    JSON.mapping(
-      size: Int32?,
-      compression: Int32?,
-      mimeType: String?,
-      text: String?,
-      encoding: String?,
-      comment: String?,
-    )
+    include JSON::Serializable
+
+    property size : Int32?
+    property compression : Int32?
+    property mimeType : String?
+    property text : String?
+    property encoding : String?
+    property comment : String?
   end
 
   class Cache
-    JSON.mapping(
-      beforeRequest: CacheRequest?,
-      afterRequest: CacheRequest?,
-      comment: String?,
-    )
+    include JSON::Serializable
+
+    property beforeRequest : CacheRequest?
+    property afterRequest : CacheRequest?
+    property comment : String?
   end
 
   class CacheRequest
-    JSON.mapping(
-      expires: String?,
-      lastAccess: String,
-      eTag: String,
-      hitCount: Int32,
-      comment: String?
-    )
+    include JSON::Serializable
+
+    property expires : String?
+    property lastAccess : String
+    property eTag : String
+    property hitCount : Int32
+    property comment : String?
   end
 
   class Timings
-    JSON.mapping(
-      blocked: Int32? | Float32? | Float64?,
-      dns: Int32? | Float32? | Float64?,
-      connect: Int32? | Float32? | Float64?,
-      send: Int32? | Float32? | Float64?,
-      wait: Int32? | Float32? | Float64?,
-      receive: Int32? | Float64? | Float32?,
-      ssl: Int32? | Float32? | Float64?,
-      comment: String?
-    )
+    include JSON::Serializable
+
+    property blocked : Int32? | Float32? | Float64?
+    property dns : Int32? | Float32? | Float64?
+    property connect : Int32? | Float32? | Float64?
+    property send : Int32? | Float32? | Float64?
+    property wait : Int32? | Float32? | Float64?
+    property receive : Int32? | Float64? | Float32?
+    property ssl : Int32? | Float32? | Float64?
+    property comment : String?
   end
 end

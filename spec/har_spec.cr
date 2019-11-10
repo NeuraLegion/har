4dfa0f4bd6ff4fa13a6efa5edc8e098fc8aa5e47
Sijawusz Pur Rahnama <sija@sija.pl>
2019-11-10 02:31:12 +0100

    Move spec data file to its own folder

--- spec/har_spec.cr
+++ spec/har_spec.cr
@@ -4,12 +4,12 @@ describe HAR do
   # TODO: Write tests
 
   it "parses HAR file" do
-    file = "#{__DIR__}/example_data.json"
+    file = "#{__DIR__}/data/example.json"
     HAR.from_file(file).should be_a(HAR::Log)
   end
 
   it "access some relevant data" do
-    file = "#{__DIR__}/example_data.json"
+    file = "#{__DIR__}/data/example.json"
     json = HAR.from_file(file)
     pp json
   end

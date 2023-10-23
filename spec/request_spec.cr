8187004152924ac7b234a4fcff93a451dcd33ebc
Damir Sharipov <dammer2k@gmail.com>
2023-10-23 14:49:35 +0300

    specs

--- spec/request_spec.cr
+++ spec/request_spec.cr
@@ -9,4 +9,45 @@ describe "Request spec" do
     request = request.to_http_request
     request.cookies.size.should eq 0
   end
+
+  context "post body" do
+    HAR::Request::FORMDATA_MIME_VALUES.each do |mime_type|
+      it "from #{mime_type} params" do
+        raw = %q<{
+        "url": "https://api.example.com/v2/file",
+        "method": "POST",
+        "headers": [
+            {
+                "name": "Authorization",
+                "value": "RELEVANT_API_KEY"
+            }
+        ],
+        "queryString": [],
+        "cookies": [],
+        "postData": {
+            "mimeType": "%{mime_type}",
+            "params": [
+                {
+                    "name": "query",
+                    "value": "mutation add_file($file: File!) {add_file_to_column (item_id: 1234567890, column_id:\"files\" file: $file) {id}}\n"
+                },
+                {
+                    "name": "map",
+                    "value": "{\"image\":\"variables.file\"}\n"
+                },
+                {
+                    "name": "image",
+                    "value": ""
+                }
+            ]
+        },
+        "headersSize": -1,
+        "bodySize": -1,
+        "httpVersion": "HTTP/1.1"
+      }> % {mime_type: mime_type}
+        request = HAR::Request.from_json(raw)
+        request.body.should eq "query=mutation+add_file%28%24file%3A+File%21%29+%7Badd_file_to_column+%28item_id%3A+1234567890%2C+column_id%3A%22files%22+file%3A+%24file%29+%7Bid%7D%7D%0A&map=%7B%22image%22%3A%22variables.file%22%7D%0A&image="
+      end
+    end
+  end
 end
